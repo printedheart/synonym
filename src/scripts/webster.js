d5084c88a8531d1ffdd3ae2ca76c04c8e1061c2e
sergey.melderis <sergey.melderis@3b1e88b2-8d32-0410-8810-11ad474eb51b>
2008-06-01 15:36:40 +0000

    Declare variables

--- src/scripts/webster.js
+++ src/scripts/webster.js
@@ -3,46 +3,47 @@
 
 function parseMainPage(lines) {
     for (i = 0; i < lines.length; i++) {
-        line = lines[i];
-        test = line.indexOf("javascript:popWin('/cgi-bin/audio.pl");
-        
+        var line = lines[i];
+        var test = line.indexOf("javascript:popWin('/cgi-bin/audio.pl");
+
         if (test != -1) {
             try {
-            downloader["linesAvailable(const QStringList &)"].disconnect(parseMainPage); 
-            startIndex = line.indexOf("/", test);
-            endIndex = line.indexOf("')", test);
-            urlPart = line.substring(startIndex, endIndex);
-            
-            newUrl = "http://www.merriam-webster.com" + urlPart;
+            downloader["linesAvailable(const QStringList &)"].disconnect(parseMainPage);
+            var startIndex = line.indexOf("/", test);
+            var endIndex = line.indexOf("')", test);
+            var urlPart = line.substring(startIndex, endIndex);
+
+            var newUrl = "http://www.merriam-webster.com" + urlPart;
             downloader["linesAvailable(const QStringList &)"].connect(parseSoundPage);
             downloader.download(newUrl);
             } catch (e) {
                 print(e);
             }
-        }       
-        
+        }
+
     }
 }
 
 function parseSoundPage(lines) {
     for (i = 0; i < lines.length; i++) {
-        line = lines[i];
-        test = line.indexOf("Click here to listen with your default audio player"); 
+        var line = lines[i];
+        var test = line.indexOf("Click here to listen with your default audio player");
         if (test != -1) {
-            startIndex = line.indexOf("http");
-            endIndex = line.indexOf("wav");
-            soundUrl = line.substring(startIndex, endIndex + 3);
+            var startIndex = line.indexOf("http");
+            var endIndex = line.indexOf("wav");
+            var soundUrl = line.substring(startIndex, endIndex + 3);
             downloader["linesAvailable(const QStringList &)"].disconnect(parseSoundPage);
             downloader.urlFound(soundUrl);
-            return;    
+            return;
         }
     }
 }
 
 function findSoundUrl(word) {
     downloader["linesAvailable(const QStringList &)"].connect(parseMainPage);
-    downloader.download("http://www.merriam-webster.com/dictionary/" + word); 
+    downloader.download("http://www.merriam-webster.com/dictionary/" + word);
 }
 
 
 function init() {}
+
